766c1df96cfe0b7d879875a295ee180b44f576a6
RafaelRufino <rafaelrufino.52@gmail.com>
2021-12-10 02:44:00 -0300

    concluindo componente de dashboar da aplicação

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { HighlightCard } from "../../Components/HighlightCard";
+import {
+  TransactionCard,
+  TransactionCardProps,
+} from "../../Components/TransactionCard";
 import {
   Container,
   Header,
@@ -9,9 +14,50 @@ import {
   UserName,
   User,
   Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionList,
 } from "./styles";
-
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
 export const Dashboard: React.FC = () => {
+  const data: DataListProps[] = [
+    {
+      id: "1",
+      type: "positive",
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign",
+      },
+      date: "13/04/2020",
+    },
+    {
+      id: "2",
+      type: "negative",
+      title: "Hamburgueria Pyzzy",
+      amount: " R$ 59,00",
+      category: {
+        name: "Alimentação",
+        icon: "coffee",
+      },
+      date: "10/04/2020",
+    },
+    {
+      id: "3",
+      type: "negative",
+      title: "Aluguel de apartamento",
+      amount: " R$ 1200,000",
+      category: {
+        name: "Casa",
+        icon: "shopping-bag",
+      },
+      date: "27/04/2020",
+    },
+  ];
   return (
     <Container>
       <Header>
@@ -30,6 +76,34 @@ export const Dashboard: React.FC = () => {
           <Icon name="power" />
         </UserWrapper>
       </Header>
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entrada"
+          amount="17.400,00"
+          lastTransaction="Ultimas entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saidas"
+          amount="1.259,00"
+          lastTransaction="Ultimas saida dia 03 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Entrada"
+          amount="16.141,00"
+          lastTransaction="01 á 16 de abril"
+        />
+      </HighlightCards>
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </Transactions>
     </Container>
   );
 };
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,8 +1,11 @@
 import styled from "styled-components/native";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { FlatList, FlatListProps } from "react-native";
 import theme from "../../global/styles/theme";
 import { Feather } from "@expo/vector-icons";
-
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+import { DataListProps } from ".";
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
@@ -13,7 +16,8 @@ export const Header = styled.View`
   height: ${RFPercentage(42)}px;
   background-color: ${({ theme }) => theme.colors.primary};
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+  flex-direction: row;
 `;
 
 export const UserWrapper = styled.View`
@@ -21,6 +25,7 @@ export const UserWrapper = styled.View`
   padding: 0 24px;
   flex-direction: row;
   justify-content: space-between;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
   align-items: center;
 `;
 
@@ -50,3 +55,38 @@ export const Icon = styled(Feather)`
   color: ${({ theme }) => theme.colors.secondary};
   font-size: ${RFValue(24)}px;
 `;
+
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 },
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
+`;
+
+/* Transactions */
+
+export const Transactions = styled.View`
+  flex: 1;
+  padding: 0 24px;
+  margin-top: ${RFPercentage(12)}px;
+`;
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.text_dark};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+  margin-bottom: 16px;
+`;
+
+export const TransactionList = styled(
+  FlatList as new (
+    props: FlatListProps<DataListProps>
+  ) => FlatList<DataListProps>
+).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: getBottomSpace(),
+  },
+})``;

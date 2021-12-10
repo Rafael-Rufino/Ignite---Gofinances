a43ed3705afad280920a770acb9be31b382bdf61
RafaelRufino <rafaelrufino.52@gmail.com>
2021-12-10 02:45:17 -0300

    concluindo componente de card de trasançoes

---
+++ src/Components/TransactionCard/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import {
+  Container,
+  Header,
+  Title,
+  Amount,
+  Category,
+  Date,
+  Footer,
+  Icon,
+  CategoryName,
+} from "./styles";
+
+interface CategoryProps {
+  name: string;
+  icon: string;
+}
+
+export interface TransactionCardProps {
+  type: "positive" | "negative";
+  title: string;
+  amount: string;
+  category: CategoryProps;
+  date: string;
+}
+
+interface Props {
+  data: TransactionCardProps;
+}
+
+export function TransactionCard({ data }: Props) {
+  return (
+    <Container>
+      <Header>
+        <Title>{data.title}</Title>
+        <Amount type={data.type}>
+          {data.type === "negative" && "-"}
+          {data.amount}
+        </Amount>
+      </Header>
+
+      <Footer>
+        <Category>
+          <Icon name={data.category.icon} />
+          <CategoryName> {data.category.name} </CategoryName>
+        </Category>
+        <Date>{data.date}</Date>
+      </Footer>
+    </Container>
+  );
+}
---
+++ src/Components/TransactionCard/styles.ts
@@ -0,0 +1,53 @@
+import styled, { css } from "styled-components/native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { Feather } from "@expo/vector-icons";
+import theme from "../../global/styles/theme";
+interface TypeProps {
+  type: "positive" | "negative";
+}
+
+export const Container = styled.View`
+  background-color: ${({ theme }) => theme.colors.shape};
+  padding: 17px 24px;
+  border-radius: 5px;
+  margin-bottom: 16px;
+`;
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.text_dark};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+`;
+
+export const Amount = styled.Text<TypeProps>`
+  color: ${({ theme, type }) =>
+    type === "negative" ? theme.colors.attention : theme.colors.success};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(20)}px;
+  margin-top: 2px;
+`;
+export const Footer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 19px;
+`;
+export const Category = styled.Text`
+  flex-direction: row;
+  align-items: center;
+`;
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(20)}px;
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+export const CategoryName = styled.Text`
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.text};
+  margin-left: 17px;
+`;
+
+export const Date = styled.Text`
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.text};
+`;
+export const Header = styled.View``;

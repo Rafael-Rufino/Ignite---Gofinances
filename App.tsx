699a0d25e8fd775b953b661dd3bb3ff6d0719b99
RafaelRufino <rafaelrufino.52@gmail.com>
2021-12-15 14:28:21 -0300

    configurando o react navigation, criando as rotas da aplicação dinamica

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Dashboard } from "./src/screens/Dashboard";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/global/styles/theme";
 import {
@@ -9,6 +8,8 @@ import {
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
 import AppLoading from "expo-app-loading";
+import { AppRoutes } from "./src/routes/app.routes";
+import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -20,7 +21,9 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
